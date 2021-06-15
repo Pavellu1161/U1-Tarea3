826b1676fbff95b25909bb023b8edecf1eb3905f
Pavellu1161 <kensy1161@gmail.com>
2021-06-15 16:32:18 -0600

    Inicio del proyecto

---
+++ BandasMusicales/BandasMusicales/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BandasMusicales.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
